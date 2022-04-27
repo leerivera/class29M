d6e0bc8e1f60524d6f548f695ba2ed1721af86f8
leerivera <leerivera80@gmail.com>
2022-04-26 21:16:20 -0400

    reduce

--- class29-materials/cascade-badge/js/main.js
+++ class29-materials/cascade-badge/js/main.js
@@ -1,8 +1,16 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+function reverseParty(partyList){
+    console.log(partyList.reverse())
+}
+reverseParty(['bubba','char','blah','rasta'])
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+function compareSquareAndCube(a,b){
+    return a.reduce((acc,c) => acc + c**2, 0) > b.reduce((acc, c)=> acc + c**3,0)
 
+}
+compareSquareAndCube([2,2,2], [2,2,2])
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
